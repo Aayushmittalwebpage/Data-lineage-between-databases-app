2ce793be1b67805e74b6dcdb710445d02923cdd2
Aayush Mittal <39221704+aayush-mittal9@users.noreply.github.com>
2021-07-11 15:36:49 +0530

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,24 @@
+global list_lineage_target
+global list_lineage_source
+global attribute1
+global attribute2
+global metric
+
+list_lineage_target=[]
+list_lineage_source=[]
+
+attribute1=[]
+attribute2=[]
+metric=[]
+
+
+
+# #these 2 list will be filled throughout the compare_two_table function and will be used in the end to create the lineage graph visualisation
+# list_lineage_source=[]
+# list_lineage_target=[]
+
+# #these 3 list will be filled throughout the compare_two_table function and will be used in the end to create the Tabular lineage report
+
+# attribute1=[]
+# attribute2=[]
+# metric=[]
